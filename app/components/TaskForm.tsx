81bcfda6793957db26376cf94934f064edc1f91e
Jack Michell <me@jmich.dev>
2024-06-12 04:55:48 -0500

    feat: Added a Task Form

---
+++ app/components/TaskForm.tsx
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import { TaskPriority, TaskStatus } from "../interfaces/Task";
+import { supabase } from "@/utils/supabase/client";
+
+const TaskForm: React.FC<{ onTaskAdded: () => void }> = ({ onTaskAdded }) => {
+  const [title, setTitle] = useState("");
+  const [priority, setPriority] = useState<TaskPriority>(TaskPriority.MEDIUM);
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const { data, error } = await supabase.from("tasks").insert([
+      {
+        title,
+        priority,
+        status: TaskStatus.NOT_STARTED,
+      },
+    ]);
+
+    if (error) {
+      console.error("Error adding task:", error);
+    } else {
+      setTitle("");
+      setPriority(TaskPriority.MEDIUM);
+      onTaskAdded();
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="flex justify-center items-center">
+      <div>
+        <label>Title:</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          required
+        />
+      </div>
+      <div>
+        <label>Priority:</label>
+        <select
+          value={priority}
+          onChange={(e) => setPriority(e.target.value as TaskPriority)}
+        >
+          <option value={TaskPriority.LOW}>Low</option>
+          <option value={TaskPriority.MEDIUM}>Medium</option>
+          <option value={TaskPriority.HIGH}>High</option>
+        </select>
+      </div>
+      <button type="submit">Add Task</button>
+    </form>
+  );
+};
+
+export default TaskForm;
